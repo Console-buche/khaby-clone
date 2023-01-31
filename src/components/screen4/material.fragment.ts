95670ac6df950f31fbd72ee3bd99e9a10ccbaa5f
sdubourg <sdubourg@tenable.com>
2023-01-31 09:12:35 +0100

    fix-conflict

---
+++ src/components/screen4/material.fragment.ts
@@ -0,0 +1,12 @@
+export const wallOfTextFragment = `
+varying vec2 vUv;  
+uniform sampler2D Mask;
+uniform float time;
+
+void main() {
+  vec4 maskTexture = texture2D(Mask, vUv);
+
+    gl_FragColor = vec4(1.);
+    gl_FragColor -= maskTexture;
+
+}`;
